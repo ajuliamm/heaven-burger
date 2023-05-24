7a6948ed8b7e2de8412401f1aeaab7e5c3120c49
ajuliamm <ajuliammelo@gmail.com>
2023-05-24 16:54:29 -0300

    rotas do navbar e criação de tela de pedidos prontos

---
+++ src/pages/FinishedOrders/Styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+export const Container = styled.div`
+    background-color: #7B451C; 
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100vw;
+    min-height: 100vh;
+    gap: 10px;
+`;
