4414b857c0a11556e27e9e4cb6268b546d25a344
ajuliamm <ajuliammelo@gmail.com>
2023-05-25 09:26:56 -0300

    iteração do resumo do pedido
    Co-authored-by: analeticiabacha <ana_leticia@geb.inatel.com>

--- src/components/OrderResume/OrderResume.js
+++ src/components/OrderResume/OrderResume.js
@@ -5,36 +5,37 @@ import Button from '../Button/Button';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import { useNavigate } from "react-router-dom";
 
-const OrderResume = () => {
+const OrderResume = ({ listOrder }) => {
 
     const navigate = useNavigate();
 
-    function backToHomeScreen (e) {
+    function backToHomeScreen(e) {
         e.preventDefault();
         navigate('/HomeWaiter');
-    } 
+    }
 
     return (
         <Container backgroundColor='#451E12 '>
 
             <H1>RESUMO DO PEDIDO</H1>
-            <Input placeholder='CLIENTE' type='text' class='client'/>
-            
+            <Input placeholder='CLIENTE' type='text' class='client' />
+
             <H3> Item / Valor / Qtd</H3>
             <ContainerItem>
-                <DivItem>
-                    <InfoItem>Hamburguer Simples</InfoItem>
-                    <InfoItem>10,00</InfoItem>
-                    <InfoItem>
-                    <DivQtd>
-                        <BtnQtd><i className="bi bi-dash-square-fill"></i></BtnQtd>
-                            <Quantity> 1 </Quantity>
-                        <BtnQtd><i className="bi bi-plus-square-fill"></i></BtnQtd>
-                    </DivQtd>
-                    </InfoItem>
-                    <Button id='trash'><i className="bi bi-trash3"></i></Button>
-                </DivItem>
-                
+                {listOrder.map(product => (
+                    <DivItem key={product.id}>
+                        <InfoItem>{product.name}</InfoItem>
+                        <InfoItem>{product.price}</InfoItem>
+                        <InfoItem>
+                            <DivQtd>
+                                <BtnQtd><i className="bi bi-dash-square-fill"></i></BtnQtd>
+                                <Quantity> 1 </Quantity>
+                                <BtnQtd><i className="bi bi-plus-square-fill"></i></BtnQtd>
+                            </DivQtd>
+                        </InfoItem>
+                        <Button id='trash'><i className="bi bi-trash3"></i></Button>
+                    </DivItem>
+                ))}
             </ContainerItem>
             <Total>Total:</Total>
             <Button id='orderResume'>ENVIAR PARA PREPARO</Button>
--- src/pages/waiter/NewOrder.js
+++ src/pages/waiter/NewOrder.js
@@ -1,16 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container } from "./Style";
 import Menu from '../../components/Menu/Menu';
 import OrderResume from '../../components/OrderResume/OrderResume';
 import Navbar from "../../components/Navbar/Navbar";
 
 const NewOrder = () => {
-
+    const [listOrder, setListOrder] = useState([]) 
     return (
         <Container backgroundColor='#7B451C'>
             <Navbar/>
-            <Menu/>
-            <OrderResume/>
+            <Menu setListOrder={setListOrder} listOrder={listOrder}/>
+            <OrderResume listOrder={listOrder}/>
         </Container>
         
     )
