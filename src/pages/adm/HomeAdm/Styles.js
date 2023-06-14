69932cec3d2af5b4c7e7c1a9054250750011570e
ajuliamm <ajuliammelo@gmail.com>
2023-06-14 10:13:10 -0300

    implementação home e navbar adm

---
+++ src/pages/adm/HomeAdm/Styles.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    background-color: ${props => props.backgroundColor}; 
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100vw;
+    min-height: 100vh;
+
+    @media(min-width: 850px) {
+        flex-direction:row;
+    }
+`;
+
+export const Main = styled.main`
+    display:flex;
+    flex-direction:column;
+    justify-content:center;
+    height:calc(100vh - 150px);
+    
+    @media(min-width: 850px) {
+        height:100vh;
+        width:calc(100vw - 100px);
+    }
+`;
